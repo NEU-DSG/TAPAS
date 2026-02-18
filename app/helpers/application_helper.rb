917231fe8f67947b2354fd74fc7141b0f7b51858
candyhazlett <candaces@bu.edu>
2026-02-17 20:43:53 -0500

    Fix 7 failing view specs
    Add missing `errors_for` helper to ApplicationHelper, which was called
    in all three _form.html.haml partials (collections, core_files, projects)
    but never defined. Stub `user_signed_in?` in the welcome view spec to
    avoid Devise::MissingWarden error in the Warden-less view spec context.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     content_for(:title) { page_title }
     page_title
   end
+
+  def errors_for(object)
+    return "" if object.errors.empty?
+
+    content_tag(:div, class: "error_explanation") do
+      content_tag(:ul) do
+        safe_join(object.errors.full_messages.map { |msg| content_tag(:li, msg) })
+      end
+    end
+  end
 end
--- spec/views/welcome/index.html.haml_spec.rb
+++ spec/views/welcome/index.html.haml_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "welcome/index.html.haml", type: :view do
+  before do
+    allow(view).to receive(:user_signed_in?).and_return(false)
+  end
+
   it "renders the welcome page" do
     render
     expect(rendered).to include("Welcome to TAPAS")
